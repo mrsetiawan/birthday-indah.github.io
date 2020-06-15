a4fd15e1c3aff78ddf075750faa28e41524c1dfe
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-06-16 00:41:14 +0700

    update

--- effect.js
+++ effect.js
@@ -7,11 +7,11 @@ $("document").ready(function() {
   $(window).resize(function() {
     vw = $(window).width() / 2;
     $("#b1,#b2,#b3,#b4,#b5").stop();
-    $("#b11").animate({ top: 240, left: vw - 250 }, 500);
+    $("#b11").animate({ top: 240, left: vw - 150 }, 500);
     $("#b22").animate({ top: 240, left: vw - 250 }, 500);
     $("#b33").animate({ top: 240, left: vw - 450 }, 500);
     $("#b44").animate({ top: 240, left: vw - 50 }, 500);
-    $("#b55").animate({ top: 240, left: vw + 50 }, 500);
+    $("#b55").animate({ top: 240, left: vw - 50 }, 500);
     // $("#b66").animate({ top: 240, left: vw + 150 }, 500);
     // $("#b77").animate({ top: 240, left: vw + 250 }, 500);
   });
@@ -178,7 +178,7 @@ $("document").ready(function() {
   $("#wish_message").click(function() {
     vw = $(window).width() / 2;
 
-    $("#b1,#b2,#b3,#b4,#b5,#b6,#b7, #b8, #b9").stop();
+    $("#b1,#b2,#b3,#b4,#b5").stop();
     $("#b1").attr("id", "b11");
     $("#b2").attr("id", "b22");
     $("#b3").attr("id", "b33");
@@ -188,12 +188,12 @@ $("document").ready(function() {
     // $("#b7").attr("id", "b77");
     // $("#b8").attr("id", "b88");
     // $("#b9").attr("id", "b99");
-    $("#b11").animate({ top: 240, left: vw - 250 }, 500);
+    $("#b11").animate({ top: 240, left: vw - 150 }, 500);
     $("#b22").animate({ top: 240, left: vw - 250 }, 500);
     $("#b33").animate({ top: 240, left: vw - 150 }, 500);
     $("#b44").animate({ top: 240, left: vw - 50 }, 500);
     $("#b55").animate({ top: 240, left: vw + 50 }, 500);
-    $("#b66").animate({ top: 240, left: vw + 150 }, 500);
+    // $("#b66").animate({ top: 240, left: vw + 150 }, 500);
     // $("#b77").animate({ top: 240, left: vw + 250 }, 500);
     // $("#b88").animate({ top: 240, left: vw + 250 }, 500);
     // $("#b99").animate({ top: 240, left: vw + 150 }, 500);
